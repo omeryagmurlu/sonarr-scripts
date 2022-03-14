8d0c7016e7079ae1c0f818f9abafc7392aaa59e7
Ömer Erdinç Yağmurlu <omeryagmurlu@gmail.com>
2022-03-14 02:50:21 +0100

    Logfile better indentation

--- src/log.ts
+++ src/log.ts
@@ -24,7 +24,7 @@ export const options = (opts: Options) => {
 
 // level 2
 export const error = (...x: any[]) => {
-    if (__opts.level >= 2) __log(chalk.red('ERROR'), ...x)
+    if (__opts.level >= 2) __log(chalk.red('ERROR  '), ...x)
 }
 
 // level 3
@@ -34,10 +34,10 @@ export const warn = (...x: any[]) => {
 
 // level 5
 export const log = (...x: any[]) => {
-    if (__opts.level >= 5) __log('LOG', ...x)
+    if (__opts.level >= 5) __log('LOG     ', ...x)
 }
 
 // level 8
 export const trace = (...x: any[]) => {
-    if (__opts.level >= 8) __log(chalk.cyan('TRACE'), ...x)
+    if (__opts.level >= 8) __log(chalk.cyan('TRACE  '), ...x)
 }
